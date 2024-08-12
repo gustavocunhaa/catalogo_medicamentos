9ca7eb43d1acc61c7f165ac71fbfcc08207a6c92
gustavo-cunha <gustavopcunhaa@gmail.com>
2024-08-12 16:27:17 -0300

    popula a base de dados local

---
+++ src/scripts/database.py
@@ -0,0 +1,48 @@
+import os
+import argparse
+from dotenv import load_dotenv
+
+import awswrangler as wr
+from sqlalchemy import create_engine
+
+class InsertData():
+
+    def __init__(self, user, password, host, port, database, s3_file):
+
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.database = database
+        self.s3_file = s3_file
+
+    def make_engine(self):
+        url = f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+        con = create_engine(url)
+        return con
+    
+    def read_s3(self):
+        file = f"s3://catalogo-medicamentos/silver/{self.s3_file}.parquet"
+        df = wr.s3.read_parquet(file)
+        return df
+    
+    def create_table(self):
+        con = self.make_engine()
+        df_table = self.read_s3().sort_values(by="medicamento_id")
+        df_table.to_sql(self.s3_file, con, if_exists="replace", index=False)
+
+
+parser = argparse.ArgumentParser(description="Insert Data")
+parser.add_argument("--file_name", required=True)
+args = parser.parse_args()
+
+load_dotenv()
+
+InsertData(
+    user=os.environ['user'],
+    password=os.environ['password'],
+    host=os.environ['host'],
+    port=os.environ['port'],
+    database=os.environ['database'],
+    s3_file=args.file_name
+).create_table()

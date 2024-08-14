a929d5dfec039ed44abca77b219851ebeaae5a4c
gustavo-cunha <gustavopcunhaa@gmail.com>
2024-08-14 13:29:06 -0300

    melhora documentacao da api

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -4,3 +4,17 @@ class ColetaInfo(BaseModel):
     lista_ids: list
     atributos: list
 
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "lista_ids": [1, 2, 3],
+                    "atributos": ['produto', 
+                                  'principio_ativo', 'tipo_produto',
+                                  'fabricante',
+                                  'especialidade', 'classe_terapeutica', 'categoria',
+                                  'codigo_barras', 'tipo_receita']
+                }
+            ]
+        }
+    }
